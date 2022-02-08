0467caff36748700780cb50db5d445e1ed1a4daf
Mike-std-cpu <hoodloom07@outlook.es>
2022-02-08 02:00:51 -0600

    kata 0 & kata 1

---
+++ kata_1_python.py
@@ -0,0 +1,12 @@
+#Fecha actual
+from datetime import date
+date.today()
+
+#print("Today's date is: " + str(date.today()))
+
+
+#Parsec en años luz
+parsec = 11
+lightyears = 3.26156 * parsec
+
+print(str(parsec) + " parsec, son " + str(lightyears) + " Años luz")
